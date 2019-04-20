f6b94849523b5be5c4fc0e8cae418442f7bc925e
Nobutaka Takushima <nobutaka.takushima@itakoh.co.jp>
2019-04-21 00:57:46 +0900

    update

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const camera = document.getElementById("camera");
+
+function bindCamera() {
+  const constraints = {
+    audio: false,
+    video: true
+  };
+  const promise = navigator.mediaDevices.getUserMedia(constraints);
+  promise.then(function(stream) {
+    camera.srcObject = stream;
+  }).catch(function(err) {
+    alert(err);
+  });
+}
+
 function playBell() {
-  var audio = document.getElementById("bell");
+  const audio = document.getElementById("bell");
   if (audio.paused) {
     audio.play();
   } else {
     audio.currentTime = 0;
   }
 }
+
+function notifyToSlack() {
+  const ctx = document.getElementById("cameracanvas").getContext("2d");
+  // impl.
+}
+
+function ring() {
+  playBell();
+  notifyToSlack();
+}
+
+bindCamera();
